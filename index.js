d1de8c675190bd39fe25df18ec66ec7e4891445b
Branson Morgan <bransox@comcast.net>
2022-08-10 20:40:41 -0400

    first letter of drivers

--- index.js
+++ index.js
@@ -2,3 +2,6 @@
 function findMatching (drivers, attribute) {
     return drivers.filter(function (driver) {return driver.toLowerCase() === attribute.toLowerCase()})
 }
+function fuzzyMatch (drivers, attribute) {
+    return drivers.filter(function(driver) {return driver.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase()})
+}
