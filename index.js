e019a3909df2dc2061a18f24ee99a0e24715db44
Branson Morgan <bransox@comcast.net>
2022-08-10 20:42:41 -0400

    commit tree 39e827b1fd21b1820d701c30b27305f30e64356c

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ function findMatching (drivers, attribute) {
 function fuzzyMatch (drivers, attribute) {
     return drivers.filter(function(driver) {return driver.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase()})
 }
+function matchName(drivers, argument) {
+    return drivers.filter(function(driver) {return driver.name === argument})
+}
